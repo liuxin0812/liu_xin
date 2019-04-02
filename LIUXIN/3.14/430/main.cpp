3a274c8da839bf3d4929479ac96e5c9a95d47ca0
liuxin1212 <1924370941@qq.com>
2019-04-03 07:30:23 +0800

    4.30

---
+++ LIUXIN/3.14/430/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double pair=3.14159;
+    cout<<"请输入圆的半径:"<<endl;
+    cin>>a;
+    cout <<"直径:"<<2*a<<endl;
+    cout <<"周长:"<<2*a*pair<<endl;
+    cout <<"面积:"<<a*a*pair<<endl;
+    return 0;
+}
