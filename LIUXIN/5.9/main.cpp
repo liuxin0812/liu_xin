9ee6f33b9d7170cfcd156824734815d206f00e97
liuxin1212 <1924370941@qq.com>
2019-04-07 16:20:25 +0800

    5.9

---
+++ LIUXIN/5.9/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+
+    int product=1;
+
+    for (int a=1;a<=15;a +=2)
+
+        product =product*a;
+
+        cout << "Product is: "<< product << endl;
+}
